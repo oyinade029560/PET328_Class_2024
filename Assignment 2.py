14fd3ef8a7d45ce8c4f20af5b14aeff98a3373eb
Akintayo Oyinade Grace <165100181+oyinade029560@users.noreply.github.com>
2024-04-08 00:47:23 +0100

    Create Assignment 2.py

---
+++ Assignment 2.py
@@ -0,0 +1,10 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/PC/AppData/Local/Programs/Python/Python312/1.py
+What is the reservoir porosity12
+What is the acreage of the reservoir10
+What is the thickness of the reservoir21
+What is the water saturation of the reservoir0.5
+What is formation volume factor of the reservoir fluid23
+The amount of oil initially in place is  425003.47826086957
